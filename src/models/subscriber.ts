b7ef076f19fcfe22c455f61c2120a01c8e0baad2
Lucas Yan <lucasyancarvalho@gmail.com>
2021-11-30 21:40:00 -0300

    New model subscriber related to selectiveProcess

---
+++ src/models/subscriber.ts
@@ -0,0 +1,26 @@
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn, Unique } from 'typeorm'
+import { SelectiveProcess } from './selective_process'
+
+@Entity()
+@Unique('unique_email_process', ['email', 'selectiveProcessId'])
+export class Subscriber {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    name: string;
+
+    @Column()
+    birth: Date; 
+
+    @Column({
+        length: 64
+    })
+    email: string;
+
+    @ManyToOne(() => SelectiveProcess, selectiveProcess => selectiveProcess.subscribers, {
+        cascade: true,
+        onDelete: 'CASCADE'
+    })
+    selectiveProcesss: SelectiveProcess;
+}
